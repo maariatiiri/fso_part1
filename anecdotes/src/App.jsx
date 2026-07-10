68113404d93bb90ab5d2c1bb685927ffe6469eb8
Maaria Tiiri <maaria.tiiri@aalto.fi>
2026-07-10 19:59:05 +0300

    1.13 done

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -13,12 +13,21 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(8).fill(0))
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button onClick={handleVote} text="vote"/>
       <Button onClick={()=>setSelected(Math.floor(Math.random() * 8))} text="next anecdote"/>
     </div>
     
